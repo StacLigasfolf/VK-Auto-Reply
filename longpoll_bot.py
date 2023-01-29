937782dafbcb55a308f71de901ff69f29378636a
Mark Liman <65871508+StacLigasfolf@users.noreply.github.com>
2023-01-29 18:18:40 +0300

    get started

---
+++ longpoll_bot.py
@@ -0,0 +1,40 @@
+from simple_bot import Bot  # базовый класс бота из файла simple_bot
+import base
+from vk_api.longpoll import VkLongPoll, VkEventType  # использование VkLongPoll и VkEventType
+
+
+class LongPollBot(Bot):
+    """
+    Бот, прослушивающий в бесконечном цикле входящие сообщения и способный отвечать на некоторые из них
+    Бот отвечает на строго заданные сообщения
+    """
+
+    # длительное подключение
+    long_poll = None
+
+    def __init__(self):
+        """
+        Иинициализация бота
+        """
+        super().__init__()
+        self.long_poll = VkLongPoll(self.vk_session)
+
+    def run_long_poll(self):
+        """
+        Запуск бота
+        """
+        for event in self.long_poll.listen():
+
+            # если пришло новое сообщение - происходит проверка текста сообщения
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+
+                # если была получена одна из заданных фраз
+                for key in event.text.lower().split():
+                    if key in base.data_set:
+                        # ответ отправляется в личные сообщения пользователя (если сообщение из личного чата)
+                        if event.from_user:
+                            self.send_message(receiver_user_id=event.user_id, message_text=base.data_set[key])
+
+                        # ответ отпрвляется в беседу (если сообщение было получено в общем чате)
+                        elif event.from_chat:
+                            self.send_message(receiver_user_id=event.chat_id, message_text="Всем привет")
